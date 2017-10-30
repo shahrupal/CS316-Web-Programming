9391a79675bd03cf9cf2807afebb4fb432636a14
Rupal Shah <rupal.shah@uky.edu>
2017-10-30 19:50:09 -0400

    if valid and existent, downloads user's request for jpg or mp3 from url

--- Project3/ShahRichardson_p3.js
+++ Project3/ShahRichardson_p3.js
@@ -1,4 +1,3 @@
-
 //Authors: Jacob Richardson & Rupal Shah
 
 var http = require('http'); //include http module/library
@@ -9,66 +8,65 @@ const hostname = 'feverfew.cs.uky.edu'; //set hostname
 var STARTPORT = 2000; //minimum port
 var ENDPORT = 30000;  //maximum port
 
-//create a random port between 2000 and 30000
-const port = Math.floor(Math.random() * (ENDPORT - STARTPORT + 1) + STARTPORT); 
+//create a random port between STARTPORT and ENDPORT
+//const port = Math.floor(Math.random() * (ENDPORT - STARTPORT + 1) + STARTPORT); 
+const port = 1092;
 
 //regular expression for url
 var regex = /[a-zA-Z0-9_]*[.][mp3|jpg]/;
 
 //loads filesystem
 var fs = require('fs');
-var temp;
 var file = "advert.jpg";
+var firstLoop = true;
 
-//req = request
-//rep = response
+//req = request, rep = response
 http.createServer(function serveURL(req, rep){
 
+	if(firstLoop == true){
+		console.log("---------------------\nServer started. Listing on " + hostname + "\n--------------------\n");
+		firstLoop = false;
+	}
+	
+	//gets the url and outputs it to the console
 	var inputURL = req.url;
-	console.log("URL Requested: " + inputURL);
+	console.log("URL Requested: " + inputURL + "\n");
 
-	inputURL = inputURL.substring(1, inputURL.length); //strips '/' character
-
-	console.log("URL without /: " + inputURL);
+	inputURL = inputURL.substring(1, inputURL.length); //strips '/' character from the front
 
 	if(inputURL.match(regex) == null){ //if URL does not match regular expression
-
+		//output an error message
 		rep.write('403 ERROR:\nInput not in correct format.\nMust only include letters, numbers, and underscores. Must also include .jpg or .mp3 extension.');
-		rep.end();
-		
+		rep.end();		
 	}
-	else{
-
+	else{//the url is valid
 		fs.exists(inputURL, function(exists){	
 
-			var random = Math.floor(Math.random() * 3);
-			var checkExtension = inputURL.substring(inputURL.length - 3, inputURL.length); //stores extension
-
 			if(exists){  //if the input file exists
-
+				var checkExtension = inputURL.substring(inputURL.length - 3, inputURL.length); //stores extension
+				var random = Math.floor(Math.random() * 3); //random integer between 0&2
+				
 				if(random === 2){  //outputs ad 33% of the time
 					giveAdvert(rep);
 				}
-				else if(checkExtension === "jpg"){ 
+				else if(checkExtension === "jpg"){ //if its a JPEG
 					giveJPG(inputURL.toString(), rep);
-					console.log("JPG: " + inputURL);
 				}
-				else if(checkExtension === "mp3"){
+				else if(checkExtension === "mp3"){ //if its a MP3
 					giveMP3(inputURL.toString(), rep);
-					console.log("MP3: " + inputURL);
 				}
 			
-				fs.readFile(file, function(err, data){
+				fs.readFile(file, function(err, data){ //reads the file
 					if(err){
 						throw(err);
 					}
-					console.log(file);
-					rep.end(data);
+					rep.end(data); //sends the file
 				});
 				
 			}
 			else{ //if the input file does not exist
 
+				//output an error message
 				rep.write('403 ERROR:\nFile not found.');
 				rep.end();
 
@@ -78,19 +76,19 @@ http.createServer(function serveURL(req, rep){
 
 }).listen(port, hostname);
 
-
+//makes the file to be returned the advertisment, and sets header appropriately 
 function giveAdvert(rep){
 	file = "advert.jpg";
 	rep.setHeader('Content-Type', 'image/jpeg');
 }
 
-
+//makes the file to be returned the JPG requested, and sets header appropriately 
 function giveJPG(jpgFile, rep){
 	file = jpgFile;
 	rep.setHeader('Content-Type', 'image/jpeg');
 }
 
-
+//makes the file to be returned the MP3 requested, and sets header appropriately 
 function giveMP3(mp3File, rep) {
 	file = mp3File;
 	rep.setHeader('Content-Type', 'audio/mpeg');
