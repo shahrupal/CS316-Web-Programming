0af511088639d2740ff6ea52c19a289e14e92e0d
Rupal Shah <rupal.shah@uky.edu>
2017-11-14 10:28:23 -0500

    created drop-down list from titles of .json file

--- Project4/SportCatalog.php
+++ Project4/SportCatalog.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>Sports Information</title>
+</head>
 <body>
+
+<h1>SPORTS!</h1>
 
 <?php
 
@@ -10,10 +15,22 @@ $data = file_get_contents("Sports.json");
 // store data as an array
 $json = json_decode($data, true);
 
+// prints out each title in .json file
+foreach($json['sport'] as $mydata){
+	echo $mydata['title'];
+}
+
 //echo '<pre>'; print_r($json); echo '</pre>';
-echo "My first PHP script!";
+//echo "My first PHP script!";
 
 ?>
+
+<!-- drop-down list with all titles from .json file -->
+<select name="title">
+	<?php foreach($json['sport'] as $mydata){ ?>
+		<option><?php echo $mydata['title']; ?></option>
+	<?php } ?>
+</select>
 
 </body>
 </html>
