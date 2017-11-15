60d01274fb24ab7379f28ff03b0bc280e826d291
Rupal Shah <rupal.shah@uky.edu>
2017-11-15 11:22:34 -0500

    submission working

--- Project4/SportCatalog.php
+++ Project4/SportCatalog.php
@@ -15,43 +15,64 @@ $data = file_get_contents("Sports.json");
 // store data as an array
 $json = json_decode($data, true);
 
-// prints out each title in .json file
-foreach($json['sport'] as $mydata){
-	echo $mydata['title'];
-}
-
-echo '<pre>'; print_r($json); echo '</pre>';
-
 ?>
 
-<!-- drop-down list with all titles from .json file -->
-<select>
-	<option>--Select Sport--</option>
-	<?php foreach($json['sport'] as $titlelist){ ?>
-		<option><?php echo $titlelist['title'];?></option>
-	<?php } ?>
-</select>
 
-<!-- drop-down list with all the results from .json file -->
-<select>
-	<option>--Select File--</option>
-	<?php foreach($json['sport'] as $resultlist){ ?>
-		<?php foreach($resultlist['results'] as $key=>$value){ ?>
-			<option><?php echo $key?></option>
+<?php
+if(isset($_GET['titlechoice']) && isset($_GET['resultchoice']) && isset($_GET['termchoice'])){
+	
+	if(empty($_GET['titlechoice'] || empty($_GET['resultchoice']))){
+		echo "Must choose from Title and Result list.";
+	}
+	else{
+		echo $_GET['titlechoice'];
+      		echo "submitted";
+	}
+}
+else{
+        echo "NOT SUBMITTED";
+}
+?>
+
+
+
+<form action="" method="GET">
+
+	<!-- drop-down list with all titles from .json file -->
+	<select name="titlechoice">
+		<option></option>
+		<?php foreach($json['sport'] as $titlelist){ ?>
+			<option><?php echo $titlelist['title'];?></option>
 		<?php } ?>
-	<?php } ?>
-</select>
+	</select>
 
-
-<!-- drop-down list with all the searchterms from .json file -->
-<select>
-	<option>--Select Search Terms--</option>
-	<?php foreach($json['sport'] as $searchlist){ ?>
-		<?php foreach($searchlist['searchterms'] as $terms) { ?>
-			<option><?php echo $terms;?></option>
+	<!-- drop-down list with all the results from .json file -->
+	<select name="resultchoice">
+		<option></option>
+		<?php foreach($json['sport'] as $resultlist){ ?>
+			<?php foreach($resultlist['results'] as $key=>$value){ ?>
+				<option><?php echo $key?></option>
+			<?php } ?>
 		<?php } ?>
-	<?php } ?>
-</select>
+	</select>
+
+
+	<!-- drop-down list with all the searchterms from .json file -->
+	<select name="termchoice">
+		<option></option>
+		<?php foreach($json['sport'] as $searchlist){ ?>
+			<?php foreach($searchlist['searchterms'] as $terms) { ?>
+				<option><?php echo $terms;?></option>
+			<?php } ?>
+		<?php } ?>
+	</select>
+
+
+	<!-- submit button -->
+	<input type="submit" value="submit">	
+
+</form>
+
 
 </body>
 </html>
