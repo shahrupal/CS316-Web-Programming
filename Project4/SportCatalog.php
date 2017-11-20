ad7d415f38c656cc4b1539bd59b9dc56e4bf4441
Rupal Shah <rupal.shah@uky.edu>
2017-11-20 13:26:32 -0500

    added more error checking

--- Project4/SportCatalog.php
+++ Project4/SportCatalog.php
@@ -15,41 +15,52 @@ $data = file_get_contents("Sports.json");
 // store data as an array
 $json = json_decode($data, true);
 
+// check if json file is valid
+if(json_last_error() == 0){
+	inputValidation($json);
+}
+else{
+	echo "<p>ERROR: json file is invalid.</p>";
+}
+
 ?>
 
 
 <?php
-// if the user enters required fields
-if(isset($_GET['titlechoice']) && !empty($_GET['titlechoice']) && isset($_GET['resultchoice']) && !empty($_GET['resultchoice']) && isset($_GET['termchoice'])){
-
-	$combination = false;
-	// search for user input for result and return key (.json file) associated with it
-	foreach($json['sport'] as $asport){
-		if($asport['title'] == $_GET['titlechoice']){
-			foreach($asport['results'] as $key=>$value){ 
-	                	if($key == $_GET['resultchoice']){
-					if(file_exists($value)){	
-						showResults($value,$_GET['termchoice']);
+function inputValidation($json){
+	
+	// if the user enters required fields
+	if(isset($_GET['titlechoice']) && !empty($_GET['titlechoice']) && isset($_GET['resultchoice']) && !empty($_GET['resultchoice']) && isset($_GET['termchoice'])){
+		$combination = false;
+		// search for user input for result and return key (.json file) associated with it
+		foreach($json['sport'] as $asport){
+			if($asport['title'] == $_GET['titlechoice']){
+				foreach($asport['results'] as $key=>$value){ 
+		                	if($key == $_GET['resultchoice']){
+						if(file_exists($value)){	
+							showResults($value,$_GET['termchoice']);
+						}
+						else{
+							echo "<p>ERROR: file does not exist.</p>";
+						}
+						$combination = true;
 					}
-					else{
-						echo "<p>ERROR: file does not exist.</p>";
-					}
-					$combination = true;
+       		         	}
+				if(!isset($value) || empty($value) || ($combination == false)){
+					echo "<p>ERROR: this combination does not exist.</p>";
 				}
-                	}
-			if(!isset($value) || empty($value) || ($combination == false)){
-				echo "<p>ERROR: this combination does not exist.</p>";
-			}
-		}	
+			}	
+		}
 	}
-}
 
-// if user does not enter required fields
-else if(empty($_GET['titlechoice']) || empty($_GET['resultchoice'])){
-	echo "<p>REQUIRED: choose a title and result.</p>";
-}
-else{
-        echo "NOT SUBMITTED";
+	// if user does not enter required fields
+	else if(empty($_GET['titlechoice']) || empty($_GET['resultchoice'])){
+		echo "<p>REQUIRED: choose a title and result.</p>";
+	}
+	else{
+	        echo "NOT SUBMITTED";
+	}
+
 }
 ?>
 
@@ -63,58 +74,64 @@ function showResults($file,$search){
 	$total = 0;
 ?>
 
-	<h2><?phpecho $search ?></h2>
-	<!-- SHOW COMMENTS -->
-	<h2>
-	<?php foreach($subjson['comments'] as $comments){
-		echo $comments;
-		echo " ";
-	} ?>
-	</h2>
-
-	<!-- SHOW GAMES -->
-	<table width=100%>
-
-	<?php foreach($subjson['games'] as $games){ ?>
-
-		<tr>	
-		<?php foreach($games as $key=>$val){ ?>		
-			
-			<?php if(empty($search) || ($key != $search)){ ?>
-				<td><?php echo $key ?>:   <?php echo $val ?></td>    		
-			<?php }
-			// BOLD SEARCH TERMS
-			else{ ?>
-				<td><b><?php echo $key ?>:  <?php echo $val ?></b></td>
-			<?php } ?>
-
-			<!-- WIN/LOSS PERCENTAGES -->
-			<?php if($key == "WinorLose"){
-				if($val == "W"){
-					$win = $win + 1;
-				}
-				else{
-					$loss = $loss + 1;
-				}
-				$total = $total + 1;
-			}?>
-
-		<?php } ?>
-		</tr>		
-			
-	<?php } ?>
-
-	</table>
+<?php	if(json_last_error() != 0){
+		echo "<p>ERROR: json file is improper.</p>";
+	}
+	else{
+?>
+		<h2><?php echo $search ?></h2>
+		<!-- SHOW COMMENTS -->
+		<h2>
+		<?php foreach($subjson['comments'] as $comments){
+			echo $comments;
+			echo " ";
+		} ?>
+		</h2>
 	
-	<!-- SUMMARY OF WINS/LOSSES -->
-	<h2>Games Won: <?php echo $win ?></h2>
-	<h2>Games Lost: <?php echo $loss ?></h2>
-	<h2>Total Number of Games Played: <?php echo $total ?></h2>
-	<h2>% of Games Won: <?php echo round((100*$win/$total),2) ?></h2>
-	<h2>% of Games Lost: <?php echo round((100*$loss/$total),2) ?></h2>
+		<!-- SHOW GAMES -->
+		<table width=100%>
+	
+		<?php foreach($subjson['games'] as $games){ ?>
+	
+			<tr>	
+			<?php foreach($games as $key=>$val){ ?>		
+				
+				<?php if(empty($search) || ($key != $search)){ ?>
+					<td><?php echo $key ?>:   <?php echo $val ?></td>    		
+				<?php }
+				// BOLD SEARCH TERMS
+				else{ ?>
+					<td><b><?php echo $key ?>:  <?php echo $val ?></b></td>
+				<?php } ?>
+	
+				<!-- WIN/LOSS PERCENTAGES -->
+				<?php if($key == "WinorLose"){
+					if($val == "W"){
+						$win = $win + 1;
+					}
+					else{
+						$loss = $loss + 1;
+					}
+					$total = $total + 1;
+				}?>
+	
+			<?php } ?>
+			</tr>		
+				
+		<?php } ?>
 
-	<?php break;?>
+		</table>
+	
+		<!-- SUMMARY OF WINS/LOSSES -->
+		<h2>Games Won: <?php echo $win ?></h2>
+		<h2>Games Lost: <?php echo $loss ?></h2>
+		<h2>Total Number of Games Played: <?php echo $total ?></h2>
+		<h2>% of Games Won: <?php echo round((100*$win/$total),2) ?></h2>
+		<h2>% of Games Lost: <?php echo round((100*$loss/$total),2) ?></h2>
 
+		<?php break;?>
+
+	<?php } ?>
 <?php } ?>
 
 <form action="" method="GET">
